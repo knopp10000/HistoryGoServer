a5d221de259dbbe45428780d77f0f1603787a3d5
knopp10000 <karl.gustafsson@live.com>
2020-04-21 13:26:35 +0200

    Initial commit

---
+++ src/main/java/com/group74/HistoryGoServer/HistoryGoServerApplication.java
@@ -0,0 +1,13 @@
+package com.group74.HistoryGoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryGoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryGoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group74/HistoryGoServer/HistoryGoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group74.HistoryGoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryGoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
